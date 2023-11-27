96ee0bd1eeedde07151e68cf45362c26fb075534
Erik Lunna <eslunna@gmail.com>
2023-11-27 23:55:07 +0100

    Reduce chances of throne wishes (from EvilHack).
    From EvilHack commit b8757058c:
    
    'The changes:
    * Before (vanilla), if the players luck was 0 or greater, they had a
    7.5% chance of getting a wish from sitting on a throne - now it's closer
    to 1.5%.
    * If the player doesn't get a wish, other good things can happen
    instead, including gaining permanent see invisible intrinsic
    * Once the player is granted a wish from a throne, the throne is
    guaranteed to disappear
    
    Wishes are supposed to be more rare to come by than in other variants,
    this commit addresses one area I had overlooked.'

--- src/sit.c
+++ src/sit.c
@@ -78,8 +78,28 @@ throne_sit_effect(void)
             if (u.uluck + rn2(5) < 0) {
                 You_feel("your luck is changing.");
                 change_luck(1);
-            } else
+            } else if (!rn2(5)) {
+                /* overall this equates to a 1.5% chance for a wish */
                 makewish();
+                /* no farming thrones for multiple wishes */
+                goto poofthrone;
+            } else {
+                if (Luck < 0 || (HSee_invisible & INTRINSIC)) {
+                    if (gl.level.flags.nommap) {
+                        pline("An awful drone fills your head!");
+                        make_confused((HConfusion & TIMEOUT) + (long) rnd(30),
+                                      FALSE);
+                    } else {
+                        pline("A clear image forms in your mind.");
+                        do_mapping();
+                    }
+                } else {
+                    /* permanent see invisible in this instance */
+                    Your("vision becomes crystal clear.");
+                    HSee_invisible |= FROMOUTSIDE;
+                    newsym(u.ux, u.uy);
+                }
+            }
             break;
         case 7:
             {
@@ -186,6 +206,7 @@ throne_sit_effect(void)
     }
 
     if (!rn2(3) && IS_THRONE(levl[u.ux][u.uy].typ)) {
+poofthrone:
         /* may have teleported */
         levl[u.ux][u.uy].typ = ROOM, levl[u.ux][u.uy].flags = 0;
         pline_The("throne vanishes in a puff of logic.");
