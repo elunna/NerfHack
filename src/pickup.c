f7689a8d00a2d132bad790d12bf2af62f97bd87a
Erik Lunna <eslunna@gmail.com>
2023-11-28 19:54:58 +0100

    Players cannot put known wands of cancellation or magical bags into bags of holding anymore (from dNetHack).
    Once a player has identified these objects, it's pretty universally expected that they will *never* intentionally put them into their bag of holding. So, let's help them out with that.
    
    I think this does some positive things for the game: 1) People won't rage quit and they will be happier. 2) Devs will see players not rage quitting and be happier. 3) Many promising games won't be wasted because of a simple mistake.
    
    I believe there are more interesting challenges for the players to face than this, so it's one I'd like to see put to bed. Keep in mind, that if players are careless with their unidentified equipment, that is entirely on them.

--- src/pickup.c
+++ src/pickup.c
@@ -2441,6 +2441,11 @@ in_container(struct obj *obj)
     } else if (obj == gc.current_container) {
         pline("That would be an interesting topological exercise.");
         return 0;
+    } else if ((Is_mbag(obj) || obj->otyp == WAN_CANCELLATION)
+             && objects[obj->otyp].oc_name_known && obj->dknown 
+               && gc.current_container->otyp == BAG_OF_HOLDING) {
+        pline("That combination is a little too explosive.");
+        return 0;
     } else if (obj->owornmask & (W_ARMOR | W_ACCESSORY)) {
         Norep("You cannot %s %s you are wearing.",
               Icebox ? "refrigerate" : "stash", something);
