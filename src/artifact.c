89eb15a88334d9b3199daf943c5395a071de42d3
Erik Lunna <eslunna@gmail.com>
2023-12-02 00:29:12 +0100

    Fix: player role Priest should not receive an edged artifact weapon via altar sacrifice (from EvilHack).

--- src/artifact.c
+++ src/artifact.c
@@ -172,7 +172,8 @@ mk_artifact(
            suitable for hero's role+race */
         if ((a->alignment == alignment || a->alignment == A_NONE)
             /* avoid enemies' equipment */
-            && (a->race == NON_PM || !race_hostile(&mons[a->race]))) {
+            && (a->race == NON_PM || !race_hostile(&mons[a->race]))
+            && (!(Role_if(PM_CLERIC) && (is_slash(a) || is_pierce(a))))) {
             /* when a role-specific first choice is available, use it */
             if (Role_if(a->role)) {
                 /* make this be the only possibility in the list */
