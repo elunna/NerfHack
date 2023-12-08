082e72a5ff249159a21ed5da15e1ddb9f7dc41c1
Erik Lunna <eslunna@gmail.com>
2023-12-08 03:58:53 +0100

    Revert "Covetous monsters will teleport to downstairs or upstairs"
    This reverts commit 2cad4ecec23d992d65f1f1c4c460bccb70b15b77.
    
    Three reasons to revert this commit:
    1) The original behavior of covetous monsters allows for easier mapping of Gehennom to find the stairs. This seems like a valid strategy that doesn't need to be nerfed.
    2) With this behavior, it's possible that covetous monsters will flee downstairs when you are on your ascension run, making for a non-fight.

--- src/wizard.c
+++ src/wizard.c
@@ -366,7 +366,7 @@ tactics(struct monst *mtmp)
             expels(mtmp, mtmp->data, TRUE);
 
         /* if wounded, hole up on or near the stairs (to block them) */
-        choose_stairs(&sx, &sy, (mtmp->m_id % 2));
+        choose_stairs(&sx, &sy, FALSE);
         mtmp->mavenge = 1; /* covetous monsters attack while fleeing */
         if (In_W_tower(mx, my, &u.uz)
             || (mtmp->iswiz && !sx && !mon_has_amulet(mtmp))) {
