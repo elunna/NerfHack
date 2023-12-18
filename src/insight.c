ba7460eb791bd1c3d40a6c420e327ada6d1cbd80
Erik Lunna <eslunna@gmail.com>
2023-12-18 12:44:37 +0100

    Fix: enlightenment message showing your skill was off.

--- src/insight.c
+++ src/insight.c
@@ -1347,7 +1347,7 @@ weapon_insight(int final)
         if (sklvl == P_ISRESTRICTED)
             Strcpy(sklvlbuf, "no");
         else
-            (void) lcase(skill_level_name(wtype, sklvlbuf));
+            (void) lcase(skill_level_name(P_SKILL(wtype), sklvlbuf));
         /* "you have no/basic/expert/master/grand-master skill with <skill>"
            or "you are unskilled/skilled in <skill>" */
         Sprintf(buf, "%s %s %s", sklvlbuf,
@@ -1385,7 +1385,8 @@ weapon_insight(int final)
                    skill_level_name() returns "Unknown" for it */
                 Strcpy(twobuf, "restricted");
             } else {
-                (void) lcase(skill_level_name(P_TWO_WEAPON_COMBAT, twobuf));
+                (void) lcase(skill_level_name(
+                    P_SKILL(P_TWO_WEAPON_COMBAT), twobuf));
             }
 
             /* keep buf[] from above in case skill levels match */
@@ -1420,7 +1421,7 @@ weapon_insight(int final)
                identical to the comparison between primary and twoweap */
             if (wtype2 != wtype) {
                 Strcpy(sknambuf2, skill_name(wtype2));
-                (void) lcase(skill_level_name(wtype2, sklvlbuf2));
+                (void) lcase(skill_level_name(P_SKILL(wtype2), sklvlbuf2));
                 verb_present = "is", verb_past = "was";
                 pfx[0] = sfx[0] = buf[0] = '\0';
                 if (twoskl < sklvl2) {
