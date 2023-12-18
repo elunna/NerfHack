01e67b7ebe5c1d7ccccd106687a8e0608c7457ed
Erik Lunna <eslunna@gmail.com>
2023-12-18 13:44:01 +0100

    Revert "Revert "Fix: player role Priest should not receive an edged artifact weapon via altar sacrifice (from EvilHack).""
    This reverts commit b89a58ce7c0bd96c47c819ab4bc1ab2e2afaa481.
    
    This also restricts cavepeople from receiving edged artifact weapons since they are both cannot have these skills unrestricted from altar gifts.

--- src/artifact.c
+++ src/artifact.c
@@ -172,7 +172,10 @@ mk_artifact(
            suitable for hero's role+race */
         if ((a->alignment == alignment || a->alignment == A_NONE)
             /* avoid enemies' equipment */
-            && (a->race == NON_PM || !race_hostile(&mons[a->race]))) {
+            && (a->race == NON_PM || !race_hostile(&mons[a->race]))
+            /* Priests and cavepeople don't get pointy/slicey weapons */
+            && (!((Role_if(PM_CLERIC) || Role_if(PM_CAVE_DWELLER)) 
+                  && (is_slash(a) || is_pierce(a))))) {
             /* when a role-specific first choice is available, use it */
             if (Role_if(a->role)) {
                 /* make this be the only possibility in the list */
