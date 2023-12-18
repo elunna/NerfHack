elunna/NerfHack

commit 59361a00fa2367797606089316501b9d23b920ea

Wearing cursed gear prints a weld message and auto-identifies.
From xNetHack commits 04a9cc026 and f6c12585e.

src/polyself.c
@@ -1974,75 +1974,75 @@ mbodypart(struct monst *mon, int part)
                               "fingertip", "foot", "hand",         "handed",
                               "head",      "leg",  "light headed", "neck",
                               "spine",     "toe",  "hair",         "blood",
-                              "lung",      "nose", "stomach" },
-        *jelly_parts[] = { "pseudopod", "dark spot", "front",
+                              "lung",      "nose", "stomach",      "torso" },
+        *jelly_parts[] = { "", "pseudopod", "dark spot", "front",
                            "pseudopod extension", "pseudopod extremity",
                            "pseudopod root", "grasp", "grasped",
                            "cerebral area", "lower pseudopod", "viscous",
                            "middle", "surface", "pseudopod extremity",
                            "ripples", "juices", "surface", "sensor",
-                           "stomach" },
-        *animal_parts[] = { "forelimb",  "eye",           "face",
+                           "stomach", "central mass" },
+        *animal_parts[] = { "", "forelimb", "eye",           "face",
                             "foreclaw",  "claw tip",      "rear claw",
                             "foreclaw",  "clawed",        "head",
                             "rear limb", "light headed",  "neck",
                             "spine",     "rear claw tip", "fur",
                             "blood",     "lung",          "nose",
-                            "stomach" },
-        *bird_parts[] = { "wing",     "eye",  "face",         "wing",
+                            "stomach",      "torso" },
+        *bird_parts[] = { "", "wing", "eye",  "face",         "wing",
                           "wing tip", "foot", "wing",         "winged",
                           "head",     "leg",  "light headed", "neck",
                           "spine",    "toe",  "feathers",     "blood",
-                          "lung",     "bill", "stomach" },
-        *horse_parts[] = { "foreleg",  "eye",           "face",
+                          "lung",     "bill", "stomach",      "body" },
+        *horse_parts[] = { "", "foreleg", "eye",           "face",
                            "forehoof", "hoof tip",      "rear hoof",
                            "forehoof", "hooved",        "head",
                            "rear leg", "light headed",  "neck",
                            "backbone", "rear hoof tip", "mane",
                            "blood",    "lung",          "nose",
-                           "stomach" },
-        *sphere_parts[] = { "appendage", "optic nerve", "body", "tentacle",
+                           "stomach",     "torso" },
+        *sphere_parts[] = { "", "appendage", "optic nerve", "body", "tentacle",
                             "tentacle tip", "lower appendage", "tentacle",
                             "tentacled", "body", "lower tentacle",
                             "rotational", "equator", "body",
                             "lower tentacle tip", "cilia", "life force",
-                            "retina", "olfactory nerve", "interior" },
-        *fungus_parts[] = { "mycelium", "visual area", "front",
+                            "retina", "olfactory nerve", "interior", "body" },
+        *fungus_parts[] = { "", "mycelium", "visual area", "front",
                             "hypha",    "hypha",       "root",
                             "strand",   "stranded",    "cap area",
                             "rhizome",  "sporulated",  "stalk",
                             "root",     "rhizome tip", "spores",
                             "juices",   "gill",        "gill",
-                            "interior" },
-        *vortex_parts[] = { "region",        "eye",           "front",
+                            "interior",     "body" },
+        *vortex_parts[] = { "", "region",    "eye",           "front",
                             "minor current", "minor current", "lower current",
                             "swirl",         "swirled",       "central core",
                             "lower current", "addled",        "center",
                             "currents",      "edge",          "currents",
                             "life force",    "center",        "leading edge",
-                            "interior" },
-        *snake_parts[] = { "vestigial limb", "eye", "face", "large scale",
+                            "interior",      "central core" },
+        *snake_parts[] = { "", "vestigial limb", "eye", "face", "large scale",
                            "large scale tip", "rear region", "scale gap",
                            "scale gapped", "head", "rear region",
                            "light headed", "neck", "length", "rear scale",
                            "scales", "blood", "lung", "forked tongue",
-                           "stomach" },
-        *worm_parts[] = { "anterior segment", "light sensitive cell",
+                           "stomach", "midsection" },
+        *worm_parts[] = { "", "anterior segment", "light sensitive cell",
                           "clitellum", "setae", "setae", "posterior segment",
                           "segment", "segmented", "anterior segment",
                           "posterior", "over stretched", "clitellum",
                           "length", "posterior setae", "setae", "blood",
-                          "skin", "prostomium", "stomach" },
+                          "skin", "prostomium", "stomach", "midsection" },
         *spider_parts[] = { "pedipalp", "eye", "face", "pedipalp", "tarsus",
                             "claw", "pedipalp", "palped", "cephalothorax",
                             "leg", "spun out", "cephalothorax", "abdomen",
                             "claw", "hair", "hemolymph", "book lung",
-                            "labrum", "digestive tract" },
+                            "labrum", "digestive tract", "abdomen" },
         *fish_parts[] = { "fin", "eye", "premaxillary", "pelvic axillary",
                           "pelvic fin", "anal fin", "pectoral fin", "finned",
                           "head", "peduncle", "played out", "gills",
                           "dorsal fin", "caudal fin", "scales", "blood",
-                          "gill", "nostril", "stomach" };
+                          "gill", "nostril", "stomach", "midsection" };
     /* claw attacks are overloaded in mons[]; most humanoids with
        such attacks should still reference hands rather than claws */
     static const char not_claws[] = {

include/extern.h
@@ -574,6 +574,7 @@ extern boolean mapxy_valid(coordxy, coordxy);
 extern const char *fingers_or_gloves(boolean);
 extern void off_msg(struct obj *);
 extern void toggle_displacement(struct obj *, long, boolean);
+extern void cursed_gear_welds(struct obj *);
 extern void set_wear(struct obj *);
 extern boolean donning(struct obj *);
 extern boolean doffing(struct obj *);

include/hack.h
@@ -151,7 +151,8 @@ enum bodypart_types {
     BLOOD     = 15,
     LUNG      = 16,
     NOSE      = 17,
-    STOMACH   = 18
+    STOMACH   = 18,
+    TORSO     = 19
 };
 
 #define MAX_BMASK 4

src/do_wear.c
@@ -44,6 +44,7 @@ static int armor_or_accessory_off(struct obj *);
 static int accessory_or_armor_on(struct obj *);
 static void already_wearing(const char *);
 static void already_wearing2(const char *, const char *);
+static int wornmask_to_bodypart(int);
 static int equip_ok(struct obj *, boolean, boolean);
 static int puton_ok(struct obj *);
 static int remove_ok(struct obj *);
@@ -163,6 +164,36 @@ toggle_displacement(
     }
 }
 
+/* Print a weld message after equipping cursed gear, and make the cursed state
+ * known.
+ * Note on sequencing: the call to this should come after the "You finish
+ * [equipping] your [gear]" message but before any other special messages from
+ * that piece of gear. As such, it needs to be in the various Foo_off()
+ * functions.
+ */
+void
+cursed_gear_welds(struct obj* obj)
+{
+    if (obj->cursed) {
+        const char* name = distant_name(obj, xname);
+        boolean the = (strncmp(name, "The ", 4)
+                        && !strncmp(The(name), "The ", 4));
+        const char* bodypart = body_part(wornmask_to_bodypart(obj->owornmask));
+        /* need to pluralize "themselves"/"itself" separately from pluralizing
+         * the body part; boots and gloves pluralize both, but a multiple
+         * wielded item shouldn't pluralize the body part */
+        /* can't use otense() here because it will try to produce "pair of boots
+         * welds itself", and we want "pair of boots weld themselves" */
+        boolean bodyplur = (obj == uarmg || obj == uarmf);
+        boolean themselves = (bodyplur || obj->quan > 1);
+        pline("%s%s weld%s to your %s!", the ? "The " : "",
+              simpleonames(obj),
+              themselves ? " themselves" : "s itself",
+              bodyplur ? makeplural(bodypart) : bodypart);
+        obj->bknown = TRUE;
+    }
+}
+
 /*
  * The Type_on() functions should be called *after* setworn().
  * The Type_off() functions call setworn() themselves.
@@ -174,6 +205,8 @@ Boots_on(void)
 {
     long oldprop =
         u.uprops[objects[uarmf->otyp].oc_oprop].extrinsic & ~WORN_BOOTS;
+
+    cursed_gear_welds(uarmf);
 
     switch (uarmf->otyp) {
     case LOW_BOOTS:
@@ -318,6 +351,8 @@ Cloak_on(void)
     long oldprop =
         u.uprops[objects[uarmc->otyp].oc_oprop].extrinsic & ~WORN_CLOAK;
 
+    cursed_gear_welds(uarmc);
+
     switch (uarmc->otyp) {
     case ORCISH_CLOAK:
     case DWARVISH_CLOAK:
@@ -429,6 +464,8 @@ Cloak_off(void)
 static int
 Helmet_on(void)
 {
+    cursed_gear_welds(uarmh);
+
     switch (uarmh->otyp) {
     case FEDORA:
     case HELMET:
@@ -569,6 +606,8 @@ Gloves_on(void)
     long oldprop =
         u.uprops[objects[uarmg->otyp].oc_oprop].extrinsic & ~WORN_GLOVES;
 
+    cursed_gear_welds(uarmg);
+
     switch (uarmg->otyp) {
     case LEATHER_GLOVES:
         break;
@@ -695,6 +734,8 @@ Gloves_off(void)
 static int
 Shield_on(void)
 {
+    cursed_gear_welds(uarms);
+
     /* no shield currently requires special handling when put on, but we
        keep this uncommented in case somebody adds a new one which does
        [reflection is handled by setting u.uprops[REFLECTION].extrinsic
@@ -745,6 +786,8 @@ Shield_off(void)
 static int
 Shirt_on(void)
 {
+    cursed_gear_welds(uarmu);
+
     /* no shirt currently requires special handling when put on, but we
        keep this uncommented in case somebody adds a new one which does */
     switch (uarmu->otyp) {
@@ -873,6 +916,8 @@ dragon_armor_handling(
 static int
 Armor_on(void)
 {
+    cursed_gear_welds(uarm);
+    
     if (!uarm) /* no known instances of !uarm here but play it safe */
         return 0;
     if (!uarm->known) {
@@ -964,6 +1009,12 @@ Amulet_on(void)
         setuswapwep((struct obj *) 0);
     else if (uamul == uquiver)
         setuqwep((struct obj *) 0);
+
+    /* avoid amulet of change's messages but NOT strangulation - print cursed
+     * msg before strangulation */
+    if (uamul->otyp != AMULET_OF_CHANGE) {
+        cursed_gear_welds(uamul);
+    }
 
     switch (uamul->otyp) {
     case AMULET_OF_ESP:
@@ -1190,6 +1241,8 @@ Ring_on(struct obj *obj)
     else if (obj == uquiver)
         setuqwep((struct obj *) 0);
 
+    cursed_gear_welds(obj);
+
     /* only mask out W_RING when we don't have both
        left and right rings of the same type */
     if ((oldprop & W_RING) != W_RING)
@@ -1415,6 +1468,8 @@ Blindf_on(struct obj *otmp)
         remove_worn_item(otmp, FALSE);
     setworn(otmp, W_TOOL);
     on_msg(otmp);
+
+    cursed_gear_welds(otmp);
 
     if (Blind && !already_blind) {
         changed = TRUE;
@@ -2396,6 +2451,39 @@ doputon(void)
     }
     otmp = getobj("put on", puton_ok, GETOBJ_NOFLAGS);
     return otmp ? accessory_or_armor_on(otmp) : ECMD_CANCEL;
+}
+
+/* Convert a wornmask to the body part that armor is worn on. */
+static int
+wornmask_to_bodypart(int wornmask)
+{
+    if (wornmask & (W_ARM | W_ARMC | W_ARMU)) {
+        return TORSO;
+    }
+    else if (wornmask & (W_ARMH | W_TOOL)) {
+        return HEAD;
+    }
+    else if (wornmask & W_ARMS) {
+        /* most shields are primarily worn on the arm; however, cursed shields
+         * block the use of the hand, so the hand has to be involved somehow */
+        return HAND;
+    }
+    else if (wornmask & (W_ARMG | W_WEP | W_SWAPWEP | W_QUIVER)) {
+        return HAND;
+    }
+    else if (wornmask & W_ARMF) {
+        return FOOT;
+    }
+    else if (wornmask & W_AMUL) {
+        return NECK;
+    }
+    else if (wornmask & W_RING) {
+        return FINGER;
+    }
+    else {
+        impossible("wornmask_to_bodypart: bad mask %x", wornmask);
+        return NO_PART;
+    }
 }
 
 /* calculate current armor class */

src/mkobj.c
@@ -1793,6 +1793,9 @@ curse(struct obj *otmp)
         if (!already_cursed)
             book_cursed(otmp);
     }
+    if (otmp->owornmask && !already_cursed) {
+        cursed_gear_welds(otmp);
+    }
     if (otmp->lamplit)
         maybe_adjust_light(otmp, old_light);
     return;

commit 19c8fb061a05e236525e6a0d939b87cc09c528a4

Fix: mbodyparts bug introduced in 59361a00f.

src/polyself.c
@@ -1975,59 +1975,59 @@ mbodypart(struct monst *mon, int part)
                               "head",      "leg",  "light headed", "neck",
                               "spine",     "toe",  "hair",         "blood",
                               "lung",      "nose", "stomach",      "torso" },
-        *jelly_parts[] = { "", "pseudopod", "dark spot", "front",
+        *jelly_parts[] = { "pseudopod", "dark spot", "front",
                            "pseudopod extension", "pseudopod extremity",
                            "pseudopod root", "grasp", "grasped",
                            "cerebral area", "lower pseudopod", "viscous",
                            "middle", "surface", "pseudopod extremity",
                            "ripples", "juices", "surface", "sensor",
                            "stomach", "central mass" },
-        *animal_parts[] = { "", "forelimb", "eye",           "face",
+        *animal_parts[] = { "forelimb", "eye",           "face",
                             "foreclaw",  "claw tip",      "rear claw",
                             "foreclaw",  "clawed",        "head",
                             "rear limb", "light headed",  "neck",
                             "spine",     "rear claw tip", "fur",
                             "blood",     "lung",          "nose",
                             "stomach",      "torso" },
-        *bird_parts[] = { "", "wing", "eye",  "face",         "wing",
+        *bird_parts[] = { "wing", "eye",  "face",         "wing",
                           "wing tip", "foot", "wing",         "winged",
                           "head",     "leg",  "light headed", "neck",
                           "spine",    "toe",  "feathers",     "blood",
                           "lung",     "bill", "stomach",      "body" },
-        *horse_parts[] = { "", "foreleg", "eye",           "face",
+        *horse_parts[] = { "foreleg", "eye",           "face",
                            "forehoof", "hoof tip",      "rear hoof",
                            "forehoof", "hooved",        "head",
                            "rear leg", "light headed",  "neck",
                            "backbone", "rear hoof tip", "mane",
                            "blood",    "lung",          "nose",
                            "stomach",     "torso" },
-        *sphere_parts[] = { "", "appendage", "optic nerve", "body", "tentacle",
+        *sphere_parts[] = { "appendage", "optic nerve", "body", "tentacle",
                             "tentacle tip", "lower appendage", "tentacle",
                             "tentacled", "body", "lower tentacle",
                             "rotational", "equator", "body",
                             "lower tentacle tip", "cilia", "life force",
                             "retina", "olfactory nerve", "interior", "body" },
-        *fungus_parts[] = { "", "mycelium", "visual area", "front",
+        *fungus_parts[] = { "mycelium", "visual area", "front",
                             "hypha",    "hypha",       "root",
                             "strand",   "stranded",    "cap area",
                             "rhizome",  "sporulated",  "stalk",
                             "root",     "rhizome tip", "spores",
                             "juices",   "gill",        "gill",
                             "interior",     "body" },
-        *vortex_parts[] = { "", "region",    "eye",           "front",
+        *vortex_parts[] = { "region",    "eye",           "front",
                             "minor current", "minor current", "lower current",
                             "swirl",         "swirled",       "central core",
                             "lower current", "addled",        "center",
                             "currents",      "edge",          "currents",
                             "life force",    "center",        "leading edge",
                             "interior",      "central core" },
-        *snake_parts[] = { "", "vestigial limb", "eye", "face", "large scale",
+        *snake_parts[] = { "vestigial limb", "eye", "face", "large scale",
                            "large scale tip", "rear region", "scale gap",
                            "scale gapped", "head", "rear region",
                            "light headed", "neck", "length", "rear scale",
                            "scales", "blood", "lung", "forked tongue",
                            "stomach", "midsection" },
-        *worm_parts[] = { "", "anterior segment", "light sensitive cell",
+        *worm_parts[] = { "anterior segment", "light sensitive cell",
                           "clitellum", "setae", "setae", "posterior segment",
                           "segment", "segmented", "anterior segment",
                           "posterior", "over stretched", "clitellum",