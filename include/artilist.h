118fd6ccad467e73d4ef03f4b8acfcb427e90077
Erik Lunna <eslunna@gmail.com>
2024-07-22 00:14:14 +0200

    Doomblade is a short sword.

--- include/artilist.h
+++ include/artilist.h
@@ -261,7 +261,7 @@ static NEARDATA struct artifact artilist[] = {
     /*** Chaotic artifacts ***/
     
     /* From SLASH'EM */
-    A("Doomblade", ORCISH_SHORT_SWORD,
+    A("Doomblade", SHORT_SWORD,
       SPFX_RESTR, 0, 0,
       PHYS(0, 20), NO_DFNS, NO_CARY, 0,
       A_CHAOTIC, NON_PM, NON_PM, 1000L, NO_COLOR, DOOMBLADE),
