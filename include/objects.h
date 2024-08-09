ee07dcf0a6e4b6b8acebb535417244e4c16c0223
Erik Lunna <eslunna@gmail.com>
2024-08-09 12:10:14 +0200

    Increased weight of all gray stones (except load and flint) to 25.

--- include/objects.h
+++ include/objects.h
@@ -1631,17 +1631,17 @@ MARKER(LAST_GLASS_GEM, WORTHLESS_VIOLET_GLASS)
  * that is currently everything between luckstones and flint
  * (inclusive).
  */
-ROCK("luckstone", "gray",  0,  10,  10, 60, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
+ROCK("luckstone", "gray",  0,  10,  25, 60, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
                                                                     LUCKSTONE),
-ROCK("healthstone", "gray",0 ,  8,  10, 60, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
+ROCK("healthstone", "gray",0 ,  8,  25, 60, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
                                                                   HEALTHSTONE),
-ROCK("whetstone", "gray",  0,   3,  10, 45, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
+ROCK("whetstone", "gray",  0,   3,  25, 45, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
                                                                     WHETSTONE),
-ROCK("foulstone", "gray",  0,   3,  10,  0, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
+ROCK("foulstone", "gray",  0,   3,  25,  0, 3, 3, 1, 10, 7, MINERAL, CLR_GRAY,
                                                                     FOULSTONE), 
 ROCK("loadstone", "gray",  0,  10, 500,  1, 3, 3, 1, 10, 6, MINERAL, CLR_GRAY,
                                                                     LOADSTONE),
-ROCK("touchstone", "gray", 0,   8,  10, 45, 3, 3, 1, 10, 6, MINERAL, CLR_GRAY,
+ROCK("touchstone", "gray", 0,   8,  25, 45, 3, 3, 1, 10, 6, MINERAL, CLR_GRAY,
                                                                   TOUCHSTONE),
 ROCK("flint", "gray",      0,  10,   2,  1, 6, 6, 0, 10, 7, MINERAL, CLR_GRAY,
                                                                     FLINT),
