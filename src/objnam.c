50554f47f86c43bd7c7e09a51ff996f91244a124
Erik Lunna <eslunna@gmail.com>
2024-08-20 13:46:48 +0200

    Identify enchantment on weapons we are expert in at XP lvl 10 and up.
    I think this change really complements the extra high training that the UnNetHack skill changes brought in. It makes reaching expert in something really pay off, so you might be able to pick out something that is highly enchanted without formally identifying it.

--- src/objnam.c
+++ src/objnam.c
@@ -675,14 +675,19 @@ xname_flags(
             else if (u.ulevel >= 7 && is_launcher(obj))
                 obj->known = 1;
         }
-    }
+    } 
+    /* Allow anyone who gets expert in a weapon skill to identify those
+     * weapons easily. We can easily justify this with the extra effort
+     * it takes to train up now. Similar to rangers, you need to be XP10+
+     */
+    else if (u.ulevel >= 10 && obj->oclass == WEAPON_CLASS
+        && P_SKILL(-objects[obj->otyp].oc_skill) >= P_EXPERT)
+        obj->known = 1;
 
     /* Cartomancers are masters of cards, they know everything about them. */
-    if (Role_if(PM_CARTOMANCER)) {
-        if (obj->otyp == RAZOR_CARD) {
-            obj->known = 1;
-            obj->bknown = 1;
-        }
+    if (Role_if(PM_CARTOMANCER) && obj->otyp == RAZOR_CARD) {
+        obj->known = 1;
+        obj->bknown = 1;
     }
 
     if (iflags.override_ID) {
