aa0aca92ec2767080a18b192ce5d0eca97432b47
Erik Lunna <eslunna@gmail.com>
2024-10-27 15:39:44 +0100

    Revert "Reduced artifact costs (from GnollHack)."
    This reverts commit 5d2cd2a673f921af2b217c22ae3b1c1143048b17.

--- src/artifact.c
+++ src/artifact.c
@@ -2771,7 +2771,7 @@ arti_cost(struct obj *otmp)
     else if (artilist[(int) otmp->oartifact].cost)
         return artilist[(int) otmp->oartifact].cost;
     else
-        return (20L * (long) objects[otmp->otyp].oc_cost);
+        return (100L * (long) objects[otmp->otyp].oc_cost);
 }
 
 staticfn uchar
