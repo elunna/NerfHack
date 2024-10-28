35113967d2c5868285647503887667dc5c95e7c0
Erik Lunna <eslunna@gmail.com>
2024-10-29 00:13:50 +0100

    Wizards never receive magic missile in their starting inventory.
    They already get one offensive spell. Make them hunt for their party trick.

--- src/u_init.c
+++ src/u_init.c
@@ -1293,6 +1293,7 @@ ini_inv_mkobj_filter(int oclass, boolean got_level1_spellbook)
            || (otyp == SCR_ENCHANT_WEAPON && Role_if(PM_MONK))
            /* wizard patch -- they already have one */
            || (otyp == SPE_FORCE_BOLT && Role_if(PM_WIZARD))
+           /* Make them hunt for it!*/
            || (otyp == SPE_MAGIC_MISSILE && Role_if(PM_WIZARD))
            /* vampirics start with regeneration */
            || (otyp == RIN_REGENERATION && Race_if(PM_VAMPIRE))
