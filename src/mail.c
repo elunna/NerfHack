7138ff6164f4480088d7b8bfa847935948474449
Erik Lunna <eslunna@gmail.com>
2024-10-28 20:50:47 +0100

    Another intro quote.

--- src/mail.c
+++ src/mail.c
@@ -875,7 +875,8 @@ get_hint(void)
             "There's no place like a trap-filled home.",
             "The first rule of NerfHack: You don't survive NerfHack.",
             "Life finds a way… but not in NerfHack.",
-            "Why so quiet, kamadan got your tongue?"
+            "Why so quiet, kamadan got your tongue?",
+            "The year is 20NN. Everyone plays NerfHack at TAS levels of perfection."
     };
 
     return hint[rn2(SIZE(hint))];
