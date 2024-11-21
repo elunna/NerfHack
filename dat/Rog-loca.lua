202a4e3711c1218817a37627e42b01544583e269
Erik Lunna <eslunna@gmail.com>
2024-11-21 17:44:50 +0100

    Fix: hardfloor tag typo in Rog-loca.lua.

--- dat/Rog-loca.lua
+++ dat/Rog-loca.lua
@@ -4,7 +4,7 @@
 --
 des.level_init({ style = "solidfill", fg = " " });
 
-des.level_flags("mazelevel", "hardflood");
+des.level_flags("mazelevel", "hardfloor");
 
 --         1         2         3         4         5         6         7
 --123456789012345678901234567890123456789012345678901234567890123456789012345
