db24b9669cdb389b3616ab5c3159bd9388368ecc
Erik Lunna <eslunna@gmail.com>
2024-11-22 23:19:06 +0100

    Increase duraction of invisibility from potions and wands.
    This is to make mummy wrapping still actually matter.

--- src/potion.c
+++ src/potion.c
@@ -1023,7 +1023,7 @@ staticfn void
 peffect_invisibility(struct obj *otmp)
 {
     boolean is_spell = (otmp->oclass == SPBOOK_CLASS);
-    int amt = (rnd(1000) + 250 * (bcsign(otmp) + 2));
+    int amt = (rnd(2500) + 500 * (bcsign(otmp) + 2));
     amt /= (otmp->odiluted ? 2 : 1);
 
     /* spell cannot penetrate mummy wrapping */
--- src/zap.c
+++ src/zap.c
@@ -3182,7 +3182,7 @@ zapyourself(struct obj *obj, boolean ordinary)
             You_feel("rather itchy under %s.", yname(uarmc));
             break;
         }
-        incr_itimeout(&HInvis, ordinary ? rn1(50, 50) : d(obj->spe, 250));
+        incr_itimeout(&HInvis, ordinary ? rn1(500, 500) : d(obj->spe, 250));
         if (msg) {
             learn_it = TRUE;
             newsym(u.ux, u.uy);
