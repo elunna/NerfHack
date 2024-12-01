0776e1d9d23f84ec1fbd46fd71202089d4f34c77
Erik Lunna <eslunna@gmail.com>
2024-12-01 11:19:49 +0100

    Fix: account for hiding monsters in mdisplacem (from xNetHack).
    From commit 57ae2fa263:
    
    'Was getting sanity check failures where a monster was concealing itself
    under nothing. I believe it's due to the mvm displacement logic, where
    the two monsters just are placed in each other's spots without checking
    for if one of them is currently hiding.
    
    Make them account for this. The monster initiating the displacement will
    call hideunder() if it's capable of that and hide if it chooses to; the
    monster being displaced is startled enough not to immediately hide, and
    always becomes revealed.'

--- src/mhitm.c
+++ src/mhitm.c
@@ -264,6 +264,12 @@ mdisplacem(
     update_monster_region(magr);
     update_monster_region(mdef);
 
+    /* the monster that moves can decide to hide in its new spot; the displaced
+     * monster is forced out of hiding even if it can hide in its new spot */
+    if (hides_under(magr->data)) {
+        hideunder(magr);
+    }
+    mdef->mundetected = 0;
     if (gv.vis && !quietly)
         pline("%s moves %s out of %s way!", Monnam(magr), mon_nam(mdef),
               is_rider(pa) ? "the" : mhis(magr));
