88045d9c518be033d987e5add51743231cd433c3
Erik Lunna <eslunna@gmail.com>
2025-01-11 06:55:44 -0500

    Make it harder for pets to go berserk.
    Instead of lower than 1/6th max HP, increase the threshold to 1/8th of their max HP.

--- src/mon.c
+++ src/mon.c
@@ -6989,12 +6989,11 @@ mon_berserk(struct monst *mtmp)
     }
     /*
      * As a distant callback to traitorous monsters, berserking also untames
-     * your pet. Use similar checks as betrayed().
-     */
+     * your pet when they are at critical health. Use similar checks as
+     * betrayed(). */
     if (mtmp->mtame) {
         if (!rn2(3) && rn2(22) > mtmp->mtame /* Roll against tameness */
-              /* Pet is at less than 1/6 HP */
-              && mtmp->mhp < (mtmp->mhpmax / 6)) {
+              && mtmp->mhp < (mtmp->mhpmax / 8)) {
             if (canseemon(mtmp))
                 pline("%s turns on you!", Monnam(mtmp));
             else
