09f124383dd6af50cf3873e52966882449bceb74
Erik Lunna <eslunna@gmail.com>
2025-01-11 20:00:57 -0500

    Fix: burdened status wasn't displaying with 3 statuslines.

--- win/tty/wintty.c
+++ win/tty/wintty.c
@@ -4265,8 +4265,8 @@ static const enum statusfields
     { BL_TITLE, BL_STR, BL_DX, BL_CO, BL_IN, BL_WI, BL_CH,
       BL_SCORE, BL_FLUSH, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD },
     { BL_ALIGN, BL_GOLD, BL_HP, BL_HPMAX, BL_ENE, BL_ENEMAX,
-      BL_AC, BL_MC, BL_XP, BL_EXP, BL_HD, BL_HUNGER, blPAD,
-      BL_CAP, BL_FLUSH, blPAD, blPAD },
+      BL_AC, BL_MC, BL_XP, BL_EXP, BL_HD, BL_HUNGER,
+      BL_CAP, BL_FLUSH, blPAD, blPAD, blPAD },
     { BL_LEVELDESC, BL_TIME, BL_CONDITION, BL_VERS, BL_FLUSH, blPAD, blPAD,
       blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD, blPAD }
 };
