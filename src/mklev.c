45b4580b9af79e702324d5fdfb967b498c318ed4
Erik Lunna <eslunna@gmail.com>
2025-01-14 23:42:14 +0100

    Altars don't start spawning until level 3.
    Copying what Evil did, seems more reasonable.

--- src/mklev.c
+++ src/mklev.c
@@ -992,7 +992,7 @@ fill_ordinary_room(
         if (!rn2(15))
             mktoilet(croom);
     }
-    if (!rn2(60))
+    if (depth(&u.uz) > 2 && !rn2(60))
         mkaltar(croom);
     if (!rn2(120))
         mkforge(croom);
