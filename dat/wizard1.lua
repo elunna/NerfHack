a2f32fc8ae17a815bc7d6245d47653d86acb53d0
Erik Lunna <eslunna@gmail.com>
2025-02-17 17:17:22 +0100

    Update top of wizard's tower.
    I really don't want to import air mechanics, it just feels like a major headache for not much real gain in gameplay. So... I'm removing the bridge that existed in EvilHack and moving Rodney into the central tower and just decorating it up a bit.

--- dat/wizard1.lua
+++ dat/wizard1.lua
@@ -6,111 +6,98 @@
 -- # The top (real) wizard level.
 -- MAZE:"wizard1",' '
 
--- Ported from EvilHack
+-- Ported from EvilHack with major modifications
 -- Converted to LUA by hackemslashem
 
 des.level_init({ style="mazegrid", bg ="-" });
 
 des.level_flags("mazelevel", "noteleport", "hardfloor", "nommap", "solidify", "shortsighted")
---0         1         2         3         4         5         6         7    7
---0123456789012345678901234567890123456789012345678901234567890123456789012345
+--0         1         2         3
+--0123456789012345678901234567890
 des.map({ halign = "center", valign = "center", map = [[
-                                                                            
-         -------                                                            
-         |.....|                                                            
-        ---LLL---                                                           
-        |.......|                                                           
-        |.......|                                                           
-    --F-----------F--                                                       
-    |...............|                                ..t                    
-    F...............|-FF--                         .......                  
-    |...|.......|...|....|L..........     ..........|.....t                 
-    |...|.......|...|....|L..    ...........  ..  ..|......                 
-    F...............|-FF--                         ..t....                  
-    |...............|                                ...                    
-    --F-----------F--                                                       
-        |.......|                                                           
-        |.......|                                                           
-        ---LLL---                                                           
-         |.....|                                                            
-         -------                                                            
-                                                                            
+00          -------          
+01          |.....|          
+02         ---...---         
+03         |.......|         
+04         |.......|         
+05     --F-----+-----F--     
+06     |,,,,,.....PPPPP|     
+07--FF-|,,,...._....PPP|-FF--
+08|...LF,....|...|....PF....|
+09|....FL....|...|....,FP...|
+10--FF-|LLL...._....,,,|-FF--
+11     |LLLLL.....,,,,,|     
+12     --F-----+-----F--     
+13         |.......|         
+14         |.......|         
+15         ---...---         
+16          |.....|          
+17          -------          
 ]] });
 
 -- TELEPORT_REGION:(06,09,07,10),(00,00,00,00)
 
+-- # random wandering monster locations
+local places = { {13,01}, {13,16} }
+shuffle(places)
+
 -- # entire tower in a region, constrains monster migration
 des.region({ region={00,00,79,19}, lit=0, type="ordinary", arrival_room=true })
 
--- # random wandering monster locations
-local places = { {03,02}, {03,17}, {24,04}, {24,15},
-                 {40,06}, {40,13}, {61,02}, {61,17} }
-shuffle(places)
-
 -- # stairs down
-des.ladder("down", 05,09)
+des.ladder("down", 06,06)
 
--- # drawbridges
-des.drawbridge({ dir="west", state="closed", x=26,y=09})
-des.drawbridge({ dir="west", state="closed", x=26,y=10})
-
-des.object("chest", 56, 10)
+des.object({ id = "chest", coord = places[2] })
 
 -- # doors
-des.door("locked",12,06)
-des.door("locked",12,13)
-des.door("locked",20,09)
-des.door("locked",20,10)
+des.door("locked",13,05)
+des.door("locked",13,12)
 
 -- # The wizard and his pet
-des.monster({ id = "Wizard of Yendor", x=55, y=10, waiting=1 })
-des.monster({ id = "hell hound", x=55, y=09, waiting=1 })
+des.monster({ id = "Wizard of Yendor", coord = places[1], waiting=1 })
+des.monster({ id = "hell hound", coord = places[2], waiting=1 })
 
 -- # The local treasure
-des.object("Book of the Dead", 55, 10)
+des.object({ id = "Book of the Dead", coord = places[1] })
 
-des.object({ id = "chest", trapped = 0, locked = 1, x = 55, y = 09,
+des.object({ id = "chest", trapped = 0, locked = 1, coord = places[1],
              contents = function()
                 -- This is converted into a zappable scroll of wishing
                 des.object("wand of wishing");
                 des.object("full healing")
+                des.object("?")
+                des.object("?")
+                des.object("+")
+                des.object("+")
              end
 });
 
--- # defenders inside the tower
-des.monster("infidel", 12,08)
-des.monster("weredemon", 12,11)
-des.monster("elven cleric", 09,04)
-des.monster("elven cleric", 15,04)
-des.monster("demilich", 15,15)
-des.monster("demilich", 09,15)
+-- # entry 
+des.monster("infidel", 13,07)
+des.monster("weredemon", 16,07)
+
+-- # north room
+des.monster("elven cleric", 10,03)
+des.monster("elven cleric", 16,03)
+
+-- # south room
+des.monster("demilich", 10,14)
+des.monster("demilich", 16,14)
 
 -- # bridge
-des.monster({ class = "H", x = 39, y = 10, asleep = 1, peaceful = 0 })
+des.monster({ class = "H", x = 20, y = 11, asleep = 1, peaceful = 0 })
 
--- # flying monsters outside
-des.monster({ class = "D", coord = places[1], peaceful = 0 })
-des.monster({ class = "D", coord = places[2], peaceful = 0 })
-des.monster({ class = "D", coord = places[3], peaceful = 0 })
-des.monster({ class = "D", coord = places[4], peaceful = 0 })
+-- east wing
+des.monster({ class = "D", x = 01, y = 08, peaceful = 0 })
+des.monster({ class = "D", x = 02, y = 08, peaceful = 0 })
+des.monster({ class = "D", x = 01, y = 09, peaceful = 0 })
+des.monster({ class = "D", x = 02, y = 09, peaceful = 0 })
 
-des.monster({ class = "V", coord = places[5], peaceful = 0 })
-des.monster({ class = "V", coord = places[6], peaceful = 0 })
-des.monster({ class = "e", coord = places[7], peaceful = 0 })
-des.monster({ class = "e", coord = places[8], peaceful = 0 })
-
--- # loot
-des.object("?",11,02)
-des.object("?",12,02)
-des.object("?",13,02)
-des.object("?",11,17)
-des.object("?",12,17)
-des.object("?",13,17)
-
-des.object("+",10,02)
-des.object("+",14,02)
-des.object("+",10,17)
-des.object("+",14,17)
+-- west wing
+des.monster({ class = "D", x = 24, y = 08, peaceful = 0 })
+des.monster({ class = "D", x = 25, y = 08, peaceful = 0 })
+des.monster({ class = "D", x = 24, y = 09, peaceful = 0 })
+des.monster({ class = "D", x = 25, y = 09, peaceful = 0 })
 
 -- # none shall pass
 des.non_diggable()
