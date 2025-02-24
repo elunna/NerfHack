b66a1592173d10a27693277a4415b9c00b1098dc
Erik Lunna <eslunna@gmail.com>
2025-02-24 20:25:36 +0100

    Add info for grung and gnoll symbols.

--- src/insight.c
+++ src/insight.c
@@ -2931,7 +2931,7 @@ vanqsort_cmp(
                internal order is ok if neither or just one is punctuation
                since letters have lower values so come out before punct */
             static const char punctclasses[] = {
-                S_LIZARD, S_EEL, S_GOLEM, S_GHOST, S_DEMON, S_HUMAN, '\0'
+                S_GRUNG, S_LIZARD, S_EEL, S_GOLEM, S_GHOST, S_DEMON, S_HUMAN, '\0'
             };
 
             if ((punct = strchr(punctclasses, mcls1)) != 0)
