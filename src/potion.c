8a7d7631acf236690d46d768dae387e2970be675
Erik Lunna <eslunna@gmail.com>
2025-06-03 20:13:14 -0400

    Dipping a unihorn in acid makes a potion of full healing.
    Since this is usually an undesirable thing and unihorns are rare as hell in NerfHack, let's up the result.

--- src/potion.c
+++ src/potion.c
@@ -2960,7 +2960,7 @@ const struct PotionRecipe potionrecipes[] = {
     { POT_WATER,            UNICORN_HORN, POT_CONFUSION,        1 },
     { POT_WATER,            UNICORN_HORN, POT_BLOOD,            1 },
     { POT_WATER,            UNICORN_HORN, POT_VAMPIRE_BLOOD,    1 },
-    { POT_HEALING,          UNICORN_HORN, POT_ACID,             1 },
+    { POT_FULL_HEALING,          UNICORN_HORN, POT_ACID,             1 },
     { POT_OIL,              POT_WATER, POT_OIL,                 1 },
     { POT_RESTORE_ABILITY,  POT_SICKNESS, POT_HEALING,          1 },
 
