427074092c574435f3246e22b96e7ebd5b46e97e
Erik Lunna <eslunna@gmail.com>
2025-10-26 00:00:46 -0400

    Revert "Revert "Objects can be completely destroyed via rusting/rotting/corroding.""
    This reverts commit fdc9a0ef53e1d78de51ca41dac81c41dd4703345.

--- src/trap.c
+++ src/trap.c
@@ -117,7 +117,7 @@ burnarmor(struct monst *victim)
         item = item->nobj;
     }
 
-#define burn_dmg(obj, descr) erode_obj(obj, descr, ERODE_BURN, EF_GREASE)
+#define burn_dmg(obj, descr) erode_obj(obj, descr, ERODE_BURN, EF_GREASE | EF_DESTROY)
     while (1) {
         switch (rn2(5)) {
         case 0:
@@ -5391,7 +5391,7 @@ acid_damage(struct obj *obj)
         obj->spe = 0;
         obj->dknown = 0;
     } else
-        erode_obj(obj, (char *) 0, ERODE_CORRODE, EF_GREASE | EF_VERBOSE);
+        erode_obj(obj, (char *) 0, ERODE_CORRODE, EF_GREASE | EF_DESTROY);
 }
 
 staticfn void
@@ -5632,7 +5632,7 @@ water_damage(
             return ER_DAMAGED;
         }
     } else {
-        return erode_obj(obj, ostr, ERODE_RUST, EF_NONE);
+        return erode_obj(obj, ostr, ERODE_RUST, EF_DESTROY);
     }
     return ER_NOTHING;
 }
