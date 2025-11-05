c082b0a7097d2f14872f5538872ae014a7297163
Erik Lunna <eslunna@gmail.com>
2025-11-05 13:01:13 -0500

    Reset Oracle level placement back to vanilla. Pushing this level also pushes Soko deeper which leads to a magnitude of difficulty that is incredibly hard to prep for.

--- dat/dungeon.lua
+++ dat/dungeon.lua
@@ -71,8 +71,8 @@ dungeon = {
          {
             name = "oracle",
             bonetag = "O",
-            base = 8,
-            range = 4,
+            base = 5,
+            range = 5,
             alignment = "neutral",
             nlevels = 3
          },
