2fde295e1900bbd08d5b6f38766a08274e157e75
Erik Lunna <erik.lunna@proton.me>
2025-11-15 20:01:10 -0500

    Fix: altars could appear cracked before level 15.

--- src/mklev.c
+++ src/mklev.c
@@ -2758,8 +2758,7 @@ mkaltar(struct mkroom *croom)
     levl[m.x][m.y].altarmask = Align2amask(al);
 
     /* Sometimes they are in poor condition */
-    if (!rn2(4) || level_difficulty() > 15
-                || svl.level.flags.naltars > 1)
+    if ((level_difficulty() > 15 || svl.level.flags.naltars > 1) && !rn2(4))
         levl[m.x][m.y].cracked = 1;
 }
 
