34ad317117b2145e9fb7eda31772387392731b8a
Erik Lunna <erik.lunna@proton.me>
2025-11-18 21:39:10 -0500

    Revert "Crowning requires 13 Luck (from dnh)."
    This reverts commit 8cb2959f24316f5df71d7db3905b1cf6efa2ce62.

--- src/pray.c
+++ src/pray.c
@@ -1529,7 +1529,7 @@ pleased(aligntyp g_align)
             case 7:
             case 8:
                 /* Crowning requires maximized luck */
-                if (u.ualign.record >= PIOUS && Luck == 13
+                if (u.ualign.record >= PIOUS
                     && u.uevent.qcompleted
                     && !u.uevent.uhand_of_elbereth) {
                     gcrownu();
