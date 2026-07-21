f3c965067bfb2be794d114980732bf557855b4bf
Erik Lunna <eslunna@gmail.com>
2026-07-21 19:38:14 +0200

    Edit the orc attack messages for more brevity.

--- src/uhitm.c
+++ src/uhitm.c
@@ -373,15 +373,15 @@ check_caitiff(struct monst *mtmp)
             if (rn2(3))
                 return;
             switch (rnd(5)) {
-            case 1: You("revel in the slaughter.");
+            case 1: You("slaughter at will.");
                 break;
-            case 2: pline("%s approves of this carnage.", u_gname());
+            case 2: pline("%s approves of your carnage.", u_gname());
                 break;
-            case 3: pline("The cries of the meek fuel your bloodlust.");
+            case 3: pline("Yes... More blood!");
                 break;
-            case 4:verbalize("Might makes right.");
+            case 4: verbalize("Might makes right!");
                 break;
-            case 5: verbalize("The weak exist only to be crushed.");
+            case 5: verbalize("Puny weaklings!");
                 break;
             }
             return;
@@ -391,29 +391,27 @@ check_caitiff(struct monst *mtmp)
             if (rn2(3))
                 return;
             switch (rnd(5)) {
-            case 1: pline("The weak flounder before you!");
+            case 1: verbalize("Flounder before me!");
                 break;
             case 2: pline_mon(mtmp, "%s struggles helplessly.", Monnam(mtmp));
                 break;
-            case 3: You("strike down the defenseless!");
+            case 3: You("strike down the weak!");
                 break;
             case 4: verbalize("No mercy!");
                 break;
-            case 5: verbalize("An easy kill!");
+            case 5: verbalize("Easy kill!");
                 break;
             }
             return;
         }
-        if (mtmp->mflee && canseemon(mtmp)) {
+        if (mtmp->mflee && canseemon(mtmp) && !rn2(3)) {
             adjalign(1);
-            if (rn2(3))
-                return;
             switch (rnd(3)) {
-            case 1: You("chase down the coward.");
+            case 1: verbalize("Coward!");
                 break;
-            case 2: verbalize("Fleeing only makes them easier to catch.");
+            case 2: verbalize("Get over here!");
                 break;
-            case 3: verbalize("A fleeing monster is an easy target.");
+            case 3: verbalize("Easy target!");
                 break;
             }
             return;
