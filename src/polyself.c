5f909f0ec2041b02c002017acad819924036a4f1
Erik Lunna <eslunna@gmail.com>
2026-07-23 00:04:48 +0200

    Remove hydration drain when grung polymorph.
    I found a vicious bug with other werefoo that caused dehydration everytime a werefoo changed form. For some reason, another werefoo's pending changes also calls set_uasmon. This seems like something that should be addressed in Vanilla. Will make a note but I think removing the hydration drain is probably the best solution for now.

--- src/polyself.c
+++ src/polyself.c
@@ -129,9 +129,6 @@ set_uasmon(void)
         if (is_grung(gy.youmonst.data)) {
             if (u.hydration == 0)
                 u.hydration = rn1(250, 250);
-        } else if (Race_if(PM_GRUNG)) {
-            /* Polymorphing costs hydration as a grung */
-            dehydrate(rn1(150, 150));
         } else {
             u.hydration = 0;
         }
