bcd57298fad4014f8f5ca56b825c0db361e53a0f
Erik Lunna <eslunna@gmail.com>
2026-08-06 16:53:55 +0200

    Revert "Monsters are never generated peaceful in Sokoban (from FIQHack)."
    This reverts commit 507df6bd64d9e29b7a5287cbed82c239081ad62f.

--- src/makemon.c
+++ src/makemon.c
@@ -1713,11 +1713,6 @@ makemon(
     mtmp->mcansee = mtmp->mcanmove = TRUE;
     mtmp->seen_resistance = M_SEEN_NOTHING;
     mtmp->mpeaceful = (mmflags & MM_ANGRY) ? FALSE : peace_minded(ptr);
-
-    /* Less trouble for the player */
-    if (In_sokoban(&u.uz))
-        mtmp->mpeaceful = 0;
-
     if ((mmflags & MM_MINVIS) != 0) /* for ^G */
         mon_set_minvis(mtmp); /* call after place_monster() */
 
