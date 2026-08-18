c38a73a2b0cc04b91d0976c9f8058f2f9b4c6044
Erik Lunna <eslunna@gmail.com>
2026-08-18 19:00:09 +0200

    Add some impossible checks for unpoly_mon.
    Trying to track down a weird hp/maxhp issue with shapeshifters.

--- src/mon.c
+++ src/mon.c
@@ -7809,6 +7809,10 @@ unpoly_monster(struct monst *mtmp)
         new_were(mtmp);
         /* Recover with 50-75% HP */
         mtmp->mhp = (mtmp->mhpmax / 2) + rn2(mtmp->mhpmax / 4 + 1);
+
+        if (mtmp->mhp > mtmp->mhpmax)
+            impossible("%s has %d curr hp, %d max hp",
+                Monnam(mtmp), mtmp->mhp, mtmp->mhpmax);
         return;
     }
 
@@ -7862,7 +7866,10 @@ unpoly_monster(struct monst *mtmp)
             newcham(mtmp, &mons[mndx], NO_NC_FLAGS);
             /* Recover with 50-75% HP */
             mtmp->mhp = (mtmp->mhpmax / 2) + rnd(mtmp->mhpmax / 4 + 1);
-            return;
+
+            if (mtmp->mhp > mtmp->mhpmax)
+                impossible("%s has %d curr hp, %d max hp",
+                    Monnam(mtmp), mtmp->mhp, mtmp->mhpmax);
         }
     } else {
         if (visible)
