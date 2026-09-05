32e9765244cdc885cc6f1eabec26a1f02c1e6a4b
Erik Lunna <erik.lunna@proton.me>
2026-09-05 15:59:44 -0400

    Restore selector for foocubi behavior.

--- include/monsters.h
+++ include/monsters.h
@@ -4974,10 +4974,7 @@
      */
     MON(NAMS("incubus", "succubus", "amorous demon"), S_DEMON,
          LVL(6, 12, 0, 70, -9), (G_NOCORPSE | 1),
-         A(ATTK(AT_BITE, AD_SSEX, 0, 0),
-           ATTK(AT_CLAW, AD_PHYS, 1, 3),
-           ATTK(AT_CLAW, AD_PHYS, 1, 3),
-           NO_ATTK, NO_ATTK, NO_ATTK),
+         SEDUCTION_ATTACKS_YES,
          SIZ(WT_HUMAN, 400, MS_SEDUCE, MZ_HUMAN),
         MR_FIRE | MR_POISON, 0, M1_HUMANOID | M1_FLY | M1_POIS,
         M2_STALK | M2_HOSTILE | M2_NASTY | M2_FLANK,
