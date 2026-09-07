eba1af58faede1efd3d6afe2bd174806a8e83a31
Erik Lunna <erik.lunna@proton.me>
2026-09-07 18:15:17 -0400

    Turn some options to opt-out; turn on weaponstatus by default.

--- src/botl.c
+++ src/botl.c
@@ -826,7 +826,7 @@ const struct conditions_t conditions[] = {
 struct condtests_t condtests[CONDITION_COUNT] = {
     /* id, useropt, opt_in or out, enabled, configchoice, testresult;
        default value for enabled is !opt_in but can get changed via options */
-    { bl_bareh,     "barehanded",  opt_in,  FALSE, FALSE, FALSE },
+    { bl_bareh,     "barehanded",  opt_out,  FALSE, FALSE, FALSE },
     { bl_blind,     "blind",       opt_out, TRUE,  FALSE, FALSE },
     { bl_rabid,     "rabid",       opt_out, TRUE,  FALSE, FALSE }, /* replaces bl_busy */
     { bl_conf,      "conf",        opt_out, TRUE,  FALSE, FALSE },
@@ -834,10 +834,10 @@ struct condtests_t condtests[CONDITION_COUNT] = {
     { bl_elf_iron,  "iron",        opt_out, TRUE,  FALSE, FALSE },
     { bl_fly,       "fly",         opt_out, TRUE,  FALSE, FALSE },
     { bl_foodpois,  "foodPois",    opt_out, TRUE,  FALSE, FALSE },
-    { bl_glowhands, "glowhands",   opt_in,  FALSE, FALSE, FALSE },
+    { bl_glowhands, "glowhands",   opt_out,  FALSE, FALSE, FALSE },
     { bl_grab,      "grab",        opt_out, TRUE,  FALSE, FALSE },
     { bl_hallu,     "hallucinat",  opt_out, TRUE,  FALSE, FALSE },
-    { bl_held,      "held",        opt_in,  FALSE, FALSE, FALSE },
+    { bl_held,      "held",        opt_out,  FALSE, FALSE, FALSE },
     { bl_phase,     "phase",       opt_out, TRUE,  FALSE, FALSE },  /* replaces bl_icy */
     { bl_inlava,    "lava",        opt_out, TRUE,  FALSE, FALSE },
     { bl_lev,       "levitate",    opt_out, TRUE,  FALSE, FALSE },
@@ -854,7 +854,7 @@ struct condtests_t condtests[CONDITION_COUNT] = {
     { bl_tethered,  "tethered",    opt_in,  FALSE, FALSE, FALSE },
     { bl_trapped,   "trap",        opt_in,  FALSE, FALSE, FALSE },
     { bl_unconsc,   "unconscious", opt_in,  FALSE, FALSE, FALSE },
-    { bl_woundedl,  "woundedlegs", opt_in,  FALSE, FALSE, FALSE },
+    { bl_woundedl,  "woundedlegs", opt_out, FALSE, FALSE, FALSE },
     { bl_holding,   "holding",     opt_in,  FALSE, FALSE, FALSE },
 };
 /* condition indexing */
